bd9a2d6ec186363a415fbbf43270b9959803a1c3
Ryan Garcia <garciaryan007@yahoo.com>
2018-02-23 12:43:56 -0800

    fixes request url by adding question mark

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -18,7 +18,7 @@ class PlayerController extends Controller
     public function get_stats()
     {
       $urlObj = [
-        'College'=> '',
+        '?College'=> '',
         'Conference'=> '',
         'Country'=> '',
         'DateFrom'=> '',
@@ -55,8 +55,8 @@ class PlayerController extends Controller
         'Weight'=> ''
       ];
       $headers = ['User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'];
-      $client = new Client(['base_uri' => 'http://stats.nba.com/stats/leaguedashplayerstats?/', 'timeout' => 5.0]);
-      $request = new Request('GET', $client->withQuery(http_build_query($urlObj)));
+      $client = new Client(['base_uri' => 'http://stats.nba.com/stats/leaguedashplayerstats/', 'timeout' => 5.0, 'headers'=> $headers]);
+      $request = new Request('GET', $client->withQuery(urldecode(http_build_query($urlObj))));
       $response = $request->getBody();
 
       dd($response);
