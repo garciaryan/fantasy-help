5dbf3d26291a7a46cca7a6991717ced39c019c4c
Ryan Garcia <garciaryan007@yahoo.com>
2018-03-01 10:21:19 -0800

    passes variables to request

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -22,45 +22,8 @@ class PlayerController extends Controller
         $client = new Client();
         $season = '2017-18';
         $seasonType = 'Regular Season';
-        $urlObj = [
-          '?College'=> '',
-          'Conference'=> '',
-          'Country'=> '',
-          'DateFrom'=> '',
-          'DateTo'=> '',
-          'Division'=> '',
-          'DraftPick'=> '',
-          'DraftYear'=> '',
-          'GameScope'=> '',
-          'GameSegment'=> '',
-          'Height'=> '',
-          'LastNGames'=> '0',
-          'LeagueID'=> '00',
-          'Location'=> '',
-          'MeasureType'=> 'Base',
-          'Month'=> '0',
-          'OpponentTeamID'=> '0',
-          'Outcome'=> '',
-          'PORound'=> '0',
-          'PaceAdjust'=> 'N',
-          'PerMode'=> 'PerGame',
-          'Period'=> '0',
-          'PlayerExperience'=> '',
-          'PlayerPosition'=> '',
-          'PlusMinus'=> 'N',
-          'Rank'=> 'N',
-          'Season'=> $season,
-          'SeasonSegment'=> '',
-          'SeasonType'=> $seasonType,
-          'ShotClockRange'=> '',
-          'StarterBench'=> '',
-          'TeamID'=> '0',
-          'VsConference'=> '',
-          'VsDivision'=> '',
-          'Weight'=> ''
-        ];
 
-        $res = $client->get('http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=', ['headers' => [
+        $res = $client->get('http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=' . $season . '&SeasonSegment=&SeasonType=' . $seasonType . '&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=', ['headers' => [
         'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
         'Accept' => 'application/json'
         ]]);
