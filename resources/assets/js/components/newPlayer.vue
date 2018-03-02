ff225ceafae2dd7332f3eb9ca58ec05bcc463f52
Ryan Garcia <garciaryan007@yahoo.com>
2018-03-02 12:25:12 -0800

    switching over to vue

---
+++ resources/assets/js/components/newPlayer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container flex-middle">
+    <div class="row">
+      <div class="col-md-6">
+        <button class="btn btn-primary" data-toggle="collapse" data-target="#add-player" id="add-player">Add Player <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
+      </div>
+      <div class="col-md-6"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-6"></div>
+      <div class="col-md-6"></div>
+    </div>
+  </div>
+</template>
--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -5,17 +5,6 @@
 
 <div class="container-fluid flex-center">
 @include('partials.sidenav')
-  <div class="container flex-middle">
-    <div class="row">
-      <div class="col-md-6">
-        <button class="btn btn-primary" data-toggle="collapse" data-target="#add-player">Add Player <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-      </div>
-      <div class="col-md-6"></div>
-    </div>
-    <div class="row">
-      <div class="col-md-6"></div>
-      <div class="col-md-6"></div>
-    </div>
-  </div>
+  <new-player></new-player>
 </div>
 @endsection
