a89011356259d872bbda0cf11ed1df3f648129f8
Ryan Garcia <garciaryan007@yahoo.com>
2018-05-04 12:20:13 -0700

    adds draggable feature to cards

--- resources/assets/js/components/newPlayer.vue
+++ resources/assets/js/components/newPlayer.vue
@@ -1,39 +1,45 @@
 <template>
-  <div class="col-md-12 flx-center">
-    <md-card v-for="player in selectedPlayersID" :key="player.id" md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <md-card-header-text>
-            <div class="md-title">{{ player.player_name }}</div>
-            <div class="md-subhead">{{ player.team_abbreviation }}</div>
-          </md-card-header-text>
-          <!-- <md-card-media>
-            <img src="" alt="">
-          </md-card-media> -->
-        </md-card-header>
-        <md-card-content>
-          <div>PTS: {{ player.pts }}</div>
-          <div>AST: {{ player.ast}}</div>
-          <div>REB: {{ player.reb }}</div>
-          <div>BLK: {{ player.blk }}</div>
-          <div>TOV: {{ player.tov }}</div>
-          <div>STL: {{ player.stl }}</div>
-          <div>Fantasy Pts: {{ player.nba_fantasy_pts }}</div>
-          <div>3PT%: {{ player.fg3_pct }}</div>
-          <div>3PA: {{ player.fg3a }}</div>
-          <div>3PM: {{ player.fg3m }}</div>
-          <div>FG%: {{ player.fg_pct }}</div>
-          <div>FGA: {{ player.fga }}</div>
-          <div>FGM: {{ player.fgm }}</div>
-          <div>Fantasy Pts Rank: {{ player.nba_fantasy_pts_rank }}</div>
-          <div>PTS Rank: {{ player.pts_rank }}</div>
-        </md-card-content>
-      </md-ripple>
-    </md-card>
+  <div class="col-md-12">
+    <draggable v-model="selectedPlayersID">
+      <transition-group class="flx-center">
+        <md-card v-for="player in selectedPlayersID" :key="player.id" md-with-hover>
+          <md-ripple>
+            <md-card-header>
+              <md-card-header-text>
+                <div class="md-title">{{ player.player_name }}</div>
+                <div class="md-subhead">{{ player.team_abbreviation }}</div>
+              </md-card-header-text>
+              <!-- <md-card-media>
+                <img src="" alt="">
+              </md-card-media> -->
+            </md-card-header>
+            <md-card-content>
+              <div>PTS: {{ player.pts }}</div>
+              <div>AST: {{ player.ast}}</div>
+              <div>REB: {{ player.reb }}</div>
+              <div>BLK: {{ player.blk }}</div>
+              <div>TOV: {{ player.tov }}</div>
+              <div>STL: {{ player.stl }}</div>
+              <div>Fantasy Pts: {{ player.nba_fantasy_pts }}</div>
+              <div>3PT%: {{ player.fg3_pct }}</div>
+              <div>3PA: {{ player.fg3a }}</div>
+              <div>3PM: {{ player.fg3m }}</div>
+              <div>FG%: {{ player.fg_pct }}</div>
+              <div>FGA: {{ player.fga }}</div>
+              <div>FGM: {{ player.fgm }}</div>
+              <div>Fantasy Pts Rank: {{ player.nba_fantasy_pts_rank }}</div>
+              <div>PTS Rank: {{ player.pts_rank }}</div>
+            </md-card-content>
+          </md-ripple>
+        </md-card>
+      </transition-group>
+    </draggable>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
 
   mounted(){
@@ -47,8 +53,8 @@ export default {
     }
   },
 
-  methods: {
-    
+  components: {
+    draggable
   }
 }
 </script>
